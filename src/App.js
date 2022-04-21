9f0bf094c8b54b6ffeef049f443930566fa1b35e
Boma Dapper <94048274+Dapper50@users.noreply.github.com>
2022-04-21 15:39:17 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
+      <div className="container">
+        <h1>Weather app</h1>
 
-      <footer>
-        This project was coded by Boma Dapper and is
-        <a
-          href="https://github.com/Dapper50/react-weather-app.git"
-          target="_blank"
-        >
-          {" "}
-          open-sourced on GitHub.
-        </a>
-      </footer>
+        <footer>
+          This project was coded by Boma Dapper and is
+          <a
+            href="https://github.com/Dapper50/react-weather-app.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
