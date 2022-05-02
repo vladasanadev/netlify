156488ceccb627b2c52d098b356714519da63065
Vlada Kandyba <hello.vladasana@gmail.com>
2022-05-02 14:22:04 +0200

    added https

--- src/Weather.js
+++ src/Weather.js
@@ -21,7 +21,7 @@ export default function Weather(props) {
   }
   function search() {
     const apiKey = "6685609d083b1ef28541a8950ea34b9f";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
   function handleSubmit(event) {
